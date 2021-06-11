3de52f0fa93fbda704f02b7ff7c7fcc7dc5d2420
Nico Schett <nicoschett@icloud.com>
2021-06-11 21:38:00 +0200

    Remove craco and all relating stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {PersistGate} from 'redux-persist/integration/react'
+import 'antd/dist/antd.css'
 
-import {
-  ConnectedPageType,
-  CMSProvider,
-  PageProvider,
-  TextField,
-  IndexField
-} from './root'
-import {persistor, store} from './store/store'
+import './cms.scss'
 
-const HomePage: ConnectedPageType = ({slug}) => {
-  return (
-    <>
-      <PageProvider typeName={HomePage.PageType} slug={slug}>
-        <TextField fieldOptions={{name: 'testfield'}} />
-        <IndexField
-          outerElement={() => <div />}
-          renderItem={(item, key, navigate) => (
-            <p key={key}>
-              Slug: {item.slug} Title: {item.title}{' '}
-              <a onClick={() => navigate()}>Goto</a>
-            </p>
-          )}
-        />
-      </PageProvider>
-    </>
-  )
-}
+export type {ConnectedPageType} from '~/types'
 
-HomePage.PageType = 'HomePage'
-HomePage.ChildPages = [HomePage]
-
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <PersistGate loading={null} persistor={persistor}>
-        <CMSProvider
-          bifrostUrls={{
-            httpUrl: 'http://localhost:8000/graphql'
-          }}
-          pages={[HomePage]}
-        />
-      </PersistGate>
-    </React.StrictMode>
-  </Provider>,
-
-  document.getElementById('root')
-)
+export {TextField, RichTextField, IndexField} from './components/fields'
+export {CMSProvider, PageProvider} from './contexts'
--- src/root.tsx
+++
@@ -1,8 +0,0 @@
-import 'antd/dist/antd.css'
-
-import './cms.scss'
-
-export type {ConnectedPageType} from '~/types'
-
-export {TextField, RichTextField, IndexField} from './components/fields'
-export {CMSProvider, PageProvider} from './contexts'
