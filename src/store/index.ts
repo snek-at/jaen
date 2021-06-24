26856fe077bf7409806493210b1a7006108becac
Nico Schett <nicoschett@icloud.com>
2021-06-24 17:36:08 +0000

    Add store index export for reducers

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import {authReducer} from './authReducer'
+import {cmsReducer} from './cmsReducer'
+import {notifyReducer} from './notifyReducer'
+
+export const reducers = {authReducer, cmsReducer, notifyReducer}
