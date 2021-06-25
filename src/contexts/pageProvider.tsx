46f269e4d01fa463812d4b5023a390af4b3999e1
Nico Schett <nicoschett@icloud.com>
2021-06-25 23:59:14 +0000

    Memo pageProvider WIP!

--- src/contexts/pageProvider.tsx
+++ src/contexts/pageProvider.tsx
@@ -14,58 +14,57 @@ export type ConnectedPageType = React.FC<{slug: string}> & IConnectedPageType
 
 type PageProviderProps = PageParamsType
 
-const PageProvider: React.FC<PageProviderProps> = ({
-  children,
-  slug,
-  typeName
-}) => {
-  const dispatch = useDispatch<store.AppDispatch>()
+const PageProvider: React.FC<PageProviderProps> = React.memo(
+  ({children, slug, typeName}) => {
+    const dispatch = useDispatch<store.AppDispatch>()
 
-  const [page, _setPage] = useState<PageParamsType>({slug, typeName})
+    const [page, _setPage] = useState<PageParamsType>({slug, typeName})
 
-  const setHiddenChildSlugs = (hiddenChildSlugs: string[]) =>
-    dispatch(setHiddenChildSlugsAction({page, hiddenChildSlugs}))
+    const setHiddenChildSlugs = (hiddenChildSlugs: string[]) =>
+      dispatch(setHiddenChildSlugsAction({page, hiddenChildSlugs}))
 
-  const index = useSelector(({cms}: store.RootState) => cms.index)
+    const index = useSelector(({cms}: store.RootState) => cms.index)
 
-  const getChildPagesFromIndex = (fixedSlug?: string) => {
-    const slug = fixedSlug || page.slug
+    const getChildPagesFromIndex = (fixedSlug?: string) => {
+      const slug = fixedSlug || page.slug
+      return (
+        index?.pages[slug]?.childSlugs.map(
+          childSlug => index.pages[childSlug]
+        ) || []
+      )
+    }
+
+    const editingHiddenSlugs = useSelector(
+      ({cms}: store.RootState) =>
+        cms.dataLayer.editing.pages[page.slug]?.hiddenChildSlugs
+    )
+    const workingHiddenSlugs = useSelector(
+      ({cms}: store.RootState) =>
+        cms.dataLayer.working.pages[page.slug]?.hiddenChildSlugs
+    )
+
+    const getHiddenSlugs = () => {
+      return editingHiddenSlugs
+        ? editingHiddenSlugs || []
+        : workingHiddenSlugs || []
+    }
+
+    useEffect(() => {
+      document.title = index?.pages[page.slug]?.title || document.title
+    }, [page])
+
     return (
-      index?.pages[slug]?.childSlugs.map(childSlug => index.pages[childSlug]) ||
-      []
+      <CMSPageContext.Provider
+        value={{
+          page,
+          getChildPagesFromIndex,
+          getHiddenSlugs,
+          setHiddenChildSlugs
+        }}>
+        {children}
+      </CMSPageContext.Provider>
     )
   }
-
-  const editingHiddenSlugs = useSelector(
-    ({cms}: store.RootState) =>
-      cms.dataLayer.editing.pages[page.slug]?.hiddenChildSlugs
-  )
-  const workingHiddenSlugs = useSelector(
-    ({cms}: store.RootState) =>
-      cms.dataLayer.working.pages[page.slug]?.hiddenChildSlugs
-  )
-
-  const getHiddenSlugs = () => {
-    return editingHiddenSlugs
-      ? editingHiddenSlugs || []
-      : workingHiddenSlugs || []
-  }
-
-  useEffect(() => {
-    document.title = index?.pages[page.slug]?.title || document.title
-  }, [page])
-
-  return (
-    <CMSPageContext.Provider
-      value={{
-        page,
-        getChildPagesFromIndex,
-        getHiddenSlugs,
-        setHiddenChildSlugs
-      }}>
-      {children}
-    </CMSPageContext.Provider>
-  )
-}
+)
 
 export default PageProvider
