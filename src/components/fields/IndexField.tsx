f04ec3b30cddad2219c7eeba7084ff6868cf3ab7
Nico schett <nicoschett@icloud.com>
2021-07-03 05:06:08 +0200

    Update licenses to fit SPDX

--- src/components/fields/IndexField.tsx
+++ src/components/fields/IndexField.tsx
@@ -1,6 +1,8 @@
 /**
  * @license
- * Copyright snek-at. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: EUPL-1.2
+ * Copyright © 2021 snek.at
  *
  * Use of this source code is governed by an EUPL-1.2 license that can be found
  * in the LICENSE file at https://snek.at/license
