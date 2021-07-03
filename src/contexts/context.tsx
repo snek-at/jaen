d1fa6f5ab8c58b19ce01d0f626fb08a2bffdeb73
Nico schett <nicoschett@icloud.com>
2021-07-03 22:44:30 +0200

    Update license tags

--- src/contexts/context.tsx
+++ src/contexts/context.tsx
@@ -2,7 +2,7 @@
  * @license
  *
  * SPDX-License-Identifier: EUPL-1.2
- * Copyright © 2021 snek.at
+ * SPDX-FileCopyrightText: Copyright © 2021 snek.at
  *
  * Use of this source code is governed by an EUPL-1.2 license that can be found
  * in the LICENSE file at https://snek.at/license
