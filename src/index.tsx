d444099eb4e505addb7b80005e0c25d45eb346ec
Nico Schett <nicoschett@icloud.com>
2021-07-04 21:09:57 +0000

    feat(craco): add craco to test the core directly through a web server
    Previously craco was located on a diffrent branch which resulted in duplicate commits.
    Now craco will be directly integrated in the main branch, which benefits the development experience.

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+/**
+ * @license
+ *
+ * SPDX-FileCopyrightText: Copyright © 2021 snek.at
+ * SPDX-License-Identifier: EUPL-1.2
+ *
+ * Use of this source code is governed by an EUPL-1.2 license that can be found
+ * in the LICENSE file at https://snek.at/license
+ */
+import ReactDOM from 'react-dom'
+
+import {CMSProvider} from './core'
+import {HomePage} from './examples/pages'
+
+ReactDOM.render(
+  <CMSProvider
+    settings={{gitRemote: 'snek-shipyard/jaen-demo'}}
+    pages={[HomePage]}
+  />,
+  document.getElementById('root')
+)
