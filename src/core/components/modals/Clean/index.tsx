ce43a215870e58d70004b66b5927e3a291d39ffd
Nico Schett <nicoschett@icloud.com>
2021-07-07 12:54:05 +0000

    refactor(core cleanmodal): add width as optional prop of CleanModal

--- src/core/components/modals/Clean/index.tsx
+++ src/core/components/modals/Clean/index.tsx
@@ -9,9 +9,13 @@
  */
 import Modal from 'antd/lib/modal/Modal'
 
-const CleanModal: React.FC = ({children}) => {
+type CleanModalProps = {width?: number}
+
+const CleanModal: React.FC<CleanModalProps> = (
+  {children, ...props} = {width: 1000}
+) => {
   return (
-    <Modal width={1000} visible title={null} footer={null} closable={false}>
+    <Modal {...props} visible title={null} footer={null} closable={false}>
       {children}
     </Modal>
   )
