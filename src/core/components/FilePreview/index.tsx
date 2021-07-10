f81f70ebe919dc10ac4fa1c4fbfafcf807c78f29
Nico Schett <nicoschett@icloud.com>
2021-07-10 22:56:21 +0000

    style(license): add missing license tags

--- src/core/components/FilePreview/index.tsx
+++ src/core/components/FilePreview/index.tsx
@@ -1,5 +1,13 @@
+/**
+ * @license
+ *
+ * SPDX-FileCopyrightText: Copyright © 2021 snek.at
+ * SPDX-License-Identifier: EUPL-1.2
+ *
+ * Use of this source code is governed by an EUPL-1.2 license that can be found
+ * in the LICENSE file at https://snek.at/license
+ */
 import {CloseOutlined, ZoomInOutlined, ZoomOutOutlined} from '@ant-design/icons'
-// import {Space} from 'antd'
 import {useEffect, useState} from 'react'
 
 import PdfViewer from '../PdfViewer'
--- src/core/components/FileUploadButton/index.tsx
+++ src/core/components/FileUploadButton/index.tsx
@@ -1,3 +1,12 @@
+/**
+ * @license
+ *
+ * SPDX-FileCopyrightText: Copyright © 2021 snek.at
+ * SPDX-License-Identifier: EUPL-1.2
+ *
+ * Use of this source code is governed by an EUPL-1.2 license that can be found
+ * in the LICENSE file at https://snek.at/license
+ */
 import {Button} from 'antd'
 import React from 'react'
 import {useDropzone} from 'react-dropzone'
