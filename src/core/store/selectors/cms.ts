fa4923f07ce1b24bf618aa54053a9993dc6b606e
Nico schett <nicoschett@icloud.com>
2021-07-14 23:05:39 +0200

    fix: update return of `pageFieldContentSelector`
    Previously `pageFieldContentSelector` returned undefined if the editing
    layer was not empty but the field was not found in it.
    In this case the respective field must be present in the working layer.
    Therefore the selector returns this field instead.

--- src/core/store/selectors/cms.ts
+++ src/core/store/selectors/cms.ts
@@ -172,7 +172,7 @@ export const pageFieldContentSelector = (
               block.blockFieldName
             ]
 
-          return blockField
+          return blockField || field
         }
       }
 
