3db04ec563230903ee962e98b36f8321f6eaf744
Nico Schett <52858351+schettn@users.noreply.github.com>
2021-07-18 06:10:10 +0200

    refactor: update publish action workflow (#14)
    * refactor: update publish action workflow
    
    This changes the publish workflow. Instead of sending the entire
    dataLayer to @snek-at/origin, it is uploaded to IPFS (encrypted).
    Afterwards the ipfs file url is send to the origin.
    This is introduced to address upload limitations.
    
    * style: fix typos
    
    * fix(common/utils.ts): syntaxError: Unterminated JSX contents (124:15)

--- src/core/common/utils.ts
+++ src/core/common/utils.ts
@@ -112,3 +112,14 @@ export const getNextIndexedObjectKey = (o: object): string => {
 
   return key
 }
+
+// Ref: https://stackoverflow.com/a/29390393
+export const blobToFile = (theBlob: Blob, fileName: string): File => {
+  var b: any = theBlob
+  //A Blob() is almost a File() - it's just missing the two properties below which we will add
+  b.lastModifiedDate = new Date()
+  b.name = fileName
+
+  //Cast to a File() type
+  return theBlob as File
+}
