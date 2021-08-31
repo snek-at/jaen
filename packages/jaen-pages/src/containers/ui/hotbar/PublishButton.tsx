2f286a9cdb6d224d1e2387307840d10eb92d450a
Nico Schett <52858351+schettn@users.noreply.github.com>
2021-08-31 16:49:36 +0000

    fix: fix incorrect import

--- packages/jaen-pages/src/containers/ui/hotbar/PublishButton.tsx
+++ packages/jaen-pages/src/containers/ui/hotbar/PublishButton.tsx
@@ -1,9 +1,7 @@
+import {useJaenCoreContext} from '@snek-at/jaen'
 import {PublishButton} from '@snek-at/jaen-shared-ui/dist/components/molecules/buttons'
 import React from 'react'
 
-import {useJaenCoreContext} from '../../../../../jaen/src'
-import {useAppDispatch, useAppSelector} from '../../../store'
-import * as actions from '../../../store/actions/optionsActions'
 import {withRedux} from '../../../store/withRedux'
 
 const Button: React.FC = () => {
