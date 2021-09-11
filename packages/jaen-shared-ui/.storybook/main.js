73996b7d89383a375ce80fc6f3a4d236875dc25a
Nico Schett <52858351+schettn@users.noreply.github.com>
2021-09-11 23:50:29 +0000

    refactor(storybook): move storybook to jaen-shared-ui package

--- packages/jaen-shared-ui/.storybook/main.js
+++ packages/jaen-shared-ui/.storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
-  stories: [
-    '../packages/*/src/**/*.stories.mdx',
-    '../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
