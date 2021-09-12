de6e0a0d02bd966775f9d8c5c6d23eaf43f37839
Nico Schett <52858351+schettn@users.noreply.github.com>
2021-09-12 13:22:47 +0000

    feat(pagecontent): update placeholder image

--- packages/jaen-shared-ui/src/components/molecules/PageContent/index.tsx
+++ packages/jaen-shared-ui/src/components/molecules/PageContent/index.tsx
@@ -163,7 +163,7 @@ const PageContent: React.FC<PageContentType> = props => {
                 h="sm"
                 w="sm"
                 src={values.image}
-                fallbackSrc="https://via.placeholder.com/150"
+                fallbackSrc="https://i.ibb.co/J2jzkBx/placeholder.jpg"
                 transition="0.2s all"
                 objectFit="cover"
                 _hover={{filter: 'brightness(70%)', cursor: 'pointer'}}
