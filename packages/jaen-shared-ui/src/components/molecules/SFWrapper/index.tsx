450ebe8e9bab631322aec6df00a586033efce931
Nico Schett <52858351+schettn@users.noreply.github.com>
2021-09-12 13:42:17 +0000

    fix(sfwrapper): show popover only on editing

--- packages/jaen-shared-ui/src/components/molecules/SFWrapper/index.tsx
+++ packages/jaen-shared-ui/src/components/molecules/SFWrapper/index.tsx
@@ -43,51 +43,49 @@ const SFWrapper: React.FC<SFWrapperProps> = ({
 
   const Wrapper = wrap ? Wrap : Box
 
-  const content = isEditing ? (
-    <Wrapper
-      {...wrapProps}
-      ref={ref}
-      boxShadow={popover.isOpen ? 'outline' : 'none'}
-      rounded="md">
-      {shouldRenderSkeleton ? <Skeleton h={20} /> : children}
-    </Wrapper>
-  ) : (
-    <Wrapper {...wrapProps}>{children}</Wrapper>
-  )
+  if (!isEditing) {
+    return <Wrapper {...wrapProps}>{children}</Wrapper>
+  }
 
   return (
-    <Box>
-      <Popover
-        trigger="hover"
-        placement="auto"
-        isOpen={popover.isOpen}
-        onOpen={popover.onOpen}
-        onClose={popover.onClose}>
-        <PopoverTrigger>{content}</PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
+    <Popover
+      trigger="hover"
+      placement="auto"
+      isOpen={popover.isOpen}
+      onOpen={popover.onOpen}
+      onClose={popover.onClose}>
+      <PopoverTrigger>
+        <Wrapper
+          {...wrapProps}
+          ref={ref}
+          boxShadow={popover.isOpen ? 'outline' : 'none'}
+          rounded="md">
+          {shouldRenderSkeleton ? <Skeleton h={20} /> : children}
+        </Wrapper>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
 
-          <PopoverHeader>{displayName}</PopoverHeader>
-          <PopoverBody>
-            <VStack align="stretch">
-              {blockTypes.map(({name, onClick}, index) => (
-                <Button
-                  key={index}
-                  leftIcon={<FaCube />}
-                  colorScheme="teal"
-                  variant="outline"
-                  size="sm"
-                  onClick={() => {
-                    onClick()
-                  }}>
-                  {name}
-                </Button>
-              ))}
-            </VStack>
-          </PopoverBody>
-        </PopoverContent>
-      </Popover>
-    </Box>
+        <PopoverHeader>{displayName}</PopoverHeader>
+        <PopoverBody>
+          <VStack align="stretch">
+            {blockTypes.map(({name, onClick}, index) => (
+              <Button
+                key={index}
+                leftIcon={<FaCube />}
+                colorScheme="teal"
+                variant="outline"
+                size="sm"
+                onClick={() => {
+                  onClick()
+                }}>
+                {name}
+              </Button>
+            ))}
+          </VStack>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
   )
 }
 
