75b3d3872ff05967ea4799b09f257723f1252270
Nico Schett <nicoschett@icloud.com>
2023-08-27 09:56:29 +0200

    feat(gatsby-jaen-lens): add icon support

--- packages/gatsby-jaen-lens/src/clients/lens/src/schema.generated.ts
+++ packages/gatsby-jaen-lens/src/clients/lens/src/schema.generated.ts
@@ -1,5 +1,10 @@
 import {proxy, arrayProxy, fnProxy, fnArrayProxy, t} from 'snek-query'
 
+export type MetaInput = {
+  label?: t.String
+  icon?: t.String
+}
+
 export class Query {
   __typename: t.String
   allService: LensService[]
@@ -13,26 +18,36 @@ export class Query {
 export class LensService {
   __typename: t.String
   id: t.String
-  label: t.Nullable<t.String>
+  meta: t.Nullable<Meta>
   fqdn: t.String
   host: t.String
   port: t.NotSupportedYet
+  isSecure: t.Boolean
   constructor() {
     this.__typename = ''
     this.id = ''
-    this.label = null
+    this.meta = proxy(Meta)
     this.fqdn = ''
     this.host = ''
     this.port = null
+    this.isSecure = false
+  }
+}
+export class Meta {
+  __typename: t.String
+  label: t.Nullable<t.String>
+  icon: t.Nullable<t.String>
+  constructor() {
+    this.__typename = ''
+    this.label = null
+    this.icon = null
   }
 }
 export class Mutation {
   __typename: t.String
-  serviceLableUpdate: (args: {id: t.String; label: t.String}) => LensService[]
-  serviceDefaultPathUpdate: (args: {id: t.String}) => LensService[]
+  serviceMetaUpdate: (args: {id: t.String; meta?: MetaInput}) => LensService[]
   constructor() {
     this.__typename = ''
-    this.serviceLableUpdate = fnArrayProxy(LensService)
-    this.serviceDefaultPathUpdate = fnArrayProxy(LensService)
+    this.serviceMetaUpdate = fnArrayProxy(LensService)
   }
 }
--- packages/gatsby-jaen-lens/src/pages/lens/index.tsx
+++ packages/gatsby-jaen-lens/src/pages/lens/index.tsx
@@ -1,8 +1,8 @@
+import {PageConfig, useNotificationsContext} from '@atsnek/jaen'
 import {
   Button,
   Heading,
   HStack,
-  IconButton,
   Input,
   Link,
   Stack,
@@ -13,13 +13,14 @@ import {
   Thead,
   Tr
 } from '@chakra-ui/react'
-import {LensService} from 'clients/lens/src/schema.generated'
-import {graphql, Link as GatsbyLink} from 'gatsby'
-import {PageConfig, useNotificationsContext} from '@atsnek/jaen'
+import {graphql} from 'gatsby'
 import {useEffect, useState} from 'react'
 import {FaEdit} from 'react-icons/fa'
+import * as SIIcons from 'react-icons/si'
 
 import {sq} from '../../clients/lens/src'
+import {LensService, MetaInput} from '../../clients/lens/src/schema.generated'
+import {IconChooser} from '../../components/IconChooser'
 
 const Page: React.FC = () => {
   const {toast} = useNotificationsContext()
@@ -32,7 +33,19 @@ const Page: React.FC = () => {
       try {
         setIsLoading(true)
 
-        const [data, errors] = await sq.query(q => q.allService)
+        const [data, errors] = await sq.query(q =>
+          q.allService.map(m => {
+            return {
+              id: m.id,
+              fqdn: m.fqdn,
+              host: m.host,
+              port: m.port,
+              meta: m.meta,
+              isSecure: m.isSecure,
+              __typename: m.__typename
+            }
+          })
+        )
         if (errors) {
           throw new Error(errors[0]?.message)
         }
@@ -51,15 +64,51 @@ const Page: React.FC = () => {
     fetchServices()
   }, [])
 
-  const updateServiceLabel = async (id: string, label: string) => {
-    const [data, errors] = await sq.mutate(m =>
-      m.serviceLableUpdate({id, label})
-    )
-    if (errors) {
-      throw new Error(errors[0]?.message)
-    }
+  const updateServiceMeta = async (
+    id: string,
+    meta: MetaInput
+  ): Promise<void> => {
+    try {
+      const [data, errors] = await sq.mutate(m =>
+        m
+          .serviceMetaUpdate({
+            id,
+            meta
+          })
+          .map(m => {
+            return {
+              id: m.id,
+              fqdn: m.fqdn,
+              host: m.host,
+              port: m.port,
+              meta: m.meta,
+              isSecure: m.isSecure,
+              __typename: m.__typename
+            }
+          })
+      )
+      if (errors) {
+        throw new Error(errors[0]?.message)
+      }
 
-    setServices(data)
+      setServices(data)
+
+      const service = data.find(s => s.id === id)
+
+      const label = service?.meta?.label || service?.id
+
+      toast({
+        title: 'Service updated',
+        description: `Service ${label} has been updated`,
+        status: 'success'
+      })
+    } catch (e) {
+      toast({
+        title: 'Error',
+        description: e.message,
+        status: 'error'
+      })
+    }
   }
 
   const [isEditing, setIsEditing] = useState(false)
@@ -97,18 +146,32 @@ const Page: React.FC = () => {
           {services.map(service => (
             <Tr key={service.id}>
               <Td>
-                {isEditing ? (
-                  <Input
-                    defaultValue={service.label || service.id}
-                    onBlur={e => {
-                      updateServiceLabel(service.id, e.target.value)
+                <HStack>
+                  <IconChooser
+                    icon={service.meta?.icon as keyof typeof SIIcons}
+                    isEditing={isEditing}
+                    setIcon={(icon: string) => {
+                      updateServiceMeta(service.id, {
+                        icon
+                      })
                     }}
                   />
-                ) : (
-                  <Link isExternal href={`https://${service.fqdn}`}>
-                    {service.label || service.id}
-                  </Link>
-                )}
+
+                  {isEditing ? (
+                    <Input
+                      defaultValue={service.meta?.label || service.id}
+                      onBlur={e => {
+                        updateServiceMeta(service.id, {
+                          label: e.target.value
+                        })
+                      }}
+                    />
+                  ) : (
+                    <Link isExternal href={`https://${service.fqdn}`}>
+                      {service.meta?.label || service.id}
+                    </Link>
+                  )}
+                </HStack>
               </Td>
               <Td>{service.host}</Td>
               <Td>{service.port}</Td>
