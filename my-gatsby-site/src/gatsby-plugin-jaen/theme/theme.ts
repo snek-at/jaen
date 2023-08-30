770360f95141df9f32dc0f03325bf9f1370f4fdf
Nico Schett <nicoschett@icloud.com>
2023-08-30 19:03:53 +0200

    chore(my-gatsby-site): update demo page

--- my-gatsby-site/src/gatsby-plugin-jaen/theme/theme.ts
+++ my-gatsby-site/src/gatsby-plugin-jaen/theme/theme.ts
@@ -8,16 +8,16 @@ import {
 export const colors = {
   ...baseTheme.colors,
   brand: {
-    ...baseTheme.colors.blue
-    // 500: '#E3000F',
-    // 600: '#E3000F'
+    ...baseTheme.colors.red,
+    500: '#E3000F',
+    600: '#E3000F'
   }
 }
 
 const theme = extendTheme(
   {colors},
   withDefaultColorScheme({
-    colorScheme: 'blue'
+    colorScheme: 'brand'
   })
 )
 
