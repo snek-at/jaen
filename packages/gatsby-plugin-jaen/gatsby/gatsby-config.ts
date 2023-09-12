ac8930ffc2ccbb4c92f7e5fae4662973f395e6dd
Nico Schett <nicoschett@icloud.com>
2023-09-12 03:04:13 +0200

    feat(gatsby-plugin-jaen): add sitemap

--- packages/gatsby-plugin-jaen/gatsby/gatsby-config.ts
+++ packages/gatsby-plugin-jaen/gatsby/gatsby-config.ts
@@ -7,7 +7,50 @@ const Config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    `gatsby-source-jaen`
+    `gatsby-source-jaen`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        excludes: [
+          '/cms',
+          '/cms/*',
+          '/login',
+          '/logout',
+          '/password_reset',
+          '/settings',
+          '/signup'
+        ],
+        query: `
+        {
+          jaenSite {
+            id
+            siteMetadata {
+              siteUrl
+            }
+          }
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+        }`,
+        resolveSiteUrl: data => {
+          console.log('RESOLVE SITE URL', data)
+          return data.jaenSite?.siteMetadata?.siteUrl || 'https://page.jaen.io'
+        },
+        resolvePages: data => {
+          return data.allSitePage.nodes.map(page => {
+            return {...page}
+          })
+        },
+        serialize: ({path, modifiedGmt}: any) => {
+          return {
+            url: path,
+            lastmod: modifiedGmt
+          }
+        }
+      }
+    }
   ]
 }
 
