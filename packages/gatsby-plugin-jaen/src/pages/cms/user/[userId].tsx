db012a75014ba7d2a9faa16e8d9dc10f4b03c44a
Florian Kleber <kleberbaum@erebos.xyz>
2023-09-13 17:48:00 +0000

    feat(gatsby-plugin-jaen): add resource user management

---
+++ packages/gatsby-plugin-jaen/src/pages/cms/user/[userId].tsx
@@ -0,0 +1,344 @@
+import { PageConfig, PageProps, useNotificationsContext } from "@atsnek/jaen";
+import { Box, Button, ButtonGroup, Flex, FormControl, FormErrorMessage, FormLabel, HStack, Input, InputGroup, InputRightElement, Skeleton, Stack, Switch, Text } from "@chakra-ui/react";
+
+
+import { navigate } from "gatsby";
+import { forwardRef, useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useUser, useUsers } from './hooks';
+
+type FormValues = {
+  emailAddress: string
+  details?: {
+    firstName?: string
+    lastName?: string
+  }
+  username: string
+  isActive: boolean
+  isAdmin: boolean
+  password?: string
+}
+
+const PasswordInput = forwardRef<HTMLInputElement, any>(
+  ({ register, ...props }, ref) => {
+    const [show, setShow] = useState(false)
+    const handleClick = () => setShow(!show)
+
+    return (
+      <InputGroup size="md">
+        <Input
+          autoComplete="new-password"
+          ref={ref}
+          pr="4.5rem"
+          type={show ? 'text' : 'password'}
+          {...props}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            {show ? 'Hide' : 'Show'}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    )
+  }
+)
+
+const Page: React.FC<PageProps> = (props) => {
+  const userId = props.params.userId ?? ''
+  const { user, isLoading } = useUser(userId)
+  const { updateUser, deleteUser } = useUsers()
+  const [changePasword, setChangePassword] = useState(false)
+
+  const defaultValues = user
+    ? {
+      emailAddress: user.primaryEmailAddress,
+      details: {
+        firstName: user.details?.firstName,
+        lastName: user.details?.lastName
+      },
+      username: user.username,
+      isActive: user.isActive,
+      isAdmin: user.isAdmin
+    }
+    : {}
+
+  console.log('defaultValues', defaultValues)
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting, isDirty, isValid }
+  } = useForm<FormValues>({
+    defaultValues
+  })
+
+  const { confirm } = useNotificationsContext()
+
+  const handleDelete = async () => {
+    const ok = await confirm({
+      title: 'Delete user',
+      message: 'Are you sure? You can\'t undo this action afterwards.',
+      confirmText: 'Delete',
+      cancelText: 'Cancel'
+    })
+
+    if (ok) {
+      const success = await deleteUser(userId)
+
+      if (success) {
+        navigate(-1)
+      }
+    }
+  }
+
+  const onReset = () => {
+    reset(defaultValues)
+
+    setChangePassword(false)
+  }
+
+  useEffect(() => {
+    onReset()
+  }, [user])
+
+  const onSubmit = async (values: FormValues) => {
+    // Get diff between old and new values typescript
+
+    const diff: any = {}
+
+    Object.keys(values).forEach(key => {
+      if ((values as any)[key] !== (defaultValues as any)[key]) {
+        diff[key] = (values as any)[key]
+      }
+    })
+
+    const success = await updateUser(userId, diff)
+
+    if (success) {
+      navigate(-1)
+
+      reset(values)
+      setChangePassword(false)
+    }
+  }
+
+  if (!user && !isLoading) {
+    return <Text>User not found</Text>
+  }
+
+  return (
+    <Box>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>ID</FormLabel>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading}>
+            <Input placeholder={user?.id} disabled />
+          </Skeleton>
+        </FormControl>
+        <FormControl mt={4} isInvalid={!!errors.emailAddress}>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>E-Mail</FormLabel>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading}>
+            <Input
+              isDisabled
+              placeholder="john.doe@snek.at"
+              {...register('emailAddress', {
+                required: 'This is required',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: 'Invalid email address'
+                }
+              })}
+            />
+          </Skeleton>
+          <FormErrorMessage>{errors.emailAddress?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl mt={4} isInvalid={!!errors.username}>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>Username</FormLabel>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading}>
+            <Input
+              placeholder="john.doe"
+              {...register('username', {
+                required: 'This is required'
+              })}
+            />
+          </Skeleton>
+          <FormErrorMessage>
+            {errors.details?.firstName?.message}
+          </FormErrorMessage>
+        </FormControl>
+
+        <Stack direction="row">
+          <Flex flex={1}>
+            <FormControl mt={4} isInvalid={!!errors.details?.lastName}>
+              <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+                <FormLabel>Firstname</FormLabel>
+              </Skeleton>
+              <Skeleton isLoaded={!isLoading}>
+                <Input placeholder="John" {...register('details.firstName', {})} />
+              </Skeleton>
+              <FormErrorMessage>
+                {errors.details?.lastName?.message}
+              </FormErrorMessage>
+            </FormControl>
+          </Flex>
+          <Flex flex={1}>
+            <FormControl mt={4} isInvalid={!!errors.details?.lastName}>
+              <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+                <FormLabel>Lastname</FormLabel>
+              </Skeleton>
+              <Skeleton isLoaded={!isLoading}>
+                <Input
+                  width={'full'}
+                  placeholder="Doe"
+                  {...register('details.lastName', {})}
+                />
+              </Skeleton>
+              <FormErrorMessage>
+                {errors.details?.lastName?.message}
+              </FormErrorMessage>
+            </FormControl>
+          </Flex>
+        </Stack>
+
+        <FormControl mt={4} isInvalid={!!errors.password}>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>Password</FormLabel>
+          </Skeleton>
+          {changePasword ? (
+            <Skeleton isLoaded={!isLoading}>
+              <PasswordInput
+                {...register('password', {
+                  required: 'This is required',
+                  pattern: {
+                    value:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    message:
+                      'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character'
+                  }
+                })}
+              />
+            </Skeleton>
+          ) : (
+            <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+              <Button onClick={() => setChangePassword(true)}>Change </Button>
+            </Skeleton>
+          )}
+          <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl mt={4} isInvalid={!!errors.isActive}>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>Active</FormLabel>
+          </Skeleton>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <Controller
+              control={control}
+              name="isActive"
+              defaultValue={user?.isActive}
+              render={({ field: { value, onChange, onBlur, ref } }) => (
+                <Switch
+                  ref={ref}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  isChecked={value}
+                />
+              )}
+            />
+          </Skeleton>
+          <FormErrorMessage>{errors.isActive?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl mt={4} isInvalid={!!errors.isAdmin}>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>Admin</FormLabel>
+          </Skeleton>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <Controller
+              control={control}
+              name="isAdmin"
+              defaultValue={user?.isAdmin}
+              render={({ field: { value, onChange, onBlur, ref } }) => (
+                <Switch
+                  ref={ref}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  isChecked={value}
+                />
+              )}
+            />
+          </Skeleton>
+          <FormErrorMessage>{errors.isAdmin?.message}</FormErrorMessage>
+        </FormControl>
+
+        <FormControl mt={4}>
+          <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+            <FormLabel>Created at</FormLabel>
+          </Skeleton>
+          <Skeleton isLoaded={!isLoading}>
+            <Input
+              placeholder={new Date(user?.createdAt ?? 0).toDateString()}
+              disabled
+            />
+          </Skeleton>
+        </FormControl>
+
+        <Box mt={8}>
+          <HStack width="full">
+            <ButtonGroup isDisabled={!isDirty}>
+              <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+                <Button type="submit" isLoading={isSubmitting}>
+                  Save Changes
+                </Button>
+              </Skeleton>
+              <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+                <Button variant="outline" onClick={onReset}>
+                  Cancel
+                </Button>
+              </Skeleton>
+            </ButtonGroup>
+            <Skeleton width={'fit-content'} isLoaded={!isLoading}>
+              <Button variant="outline" onClick={handleDelete}>
+                Delete
+              </Button>
+            </Skeleton>
+          </HStack>
+        </Box>
+      </form >
+    </Box >
+  )
+}
+
+export default Page
+
+export const pageConfig: PageConfig = {
+  label: 'Update user',
+  icon: 'FaUserCog',
+  layout: {
+    name: 'jaen',
+    type: 'form',
+  },
+  breadcrumbs: [
+    {
+      label: 'CMS',
+      path: '/cms/'
+    },
+    {
+      label: 'User',
+      path: '/cms/user/'
+    },
+    {
+      label: 'Update',
+      path: '/cms/user/'
+    }
+  ],
+
+}
---
+++ packages/gatsby-plugin-jaen/src/pages/cms/user/index.tsx
@@ -0,0 +1,384 @@
+import {
+  PageConfig
+} from '@atsnek/jaen'
+import { AddIcon, CheckCircleIcon, EditIcon } from '@chakra-ui/icons'
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  HStack,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Skeleton,
+  Stack,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure
+} from '@chakra-ui/react'
+import { graphql } from 'gatsby'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Link } from '../../../components/shared/Link'
+
+import { Mutation } from '@snek-functions/origin/dist/schema.generated'
+import { useUsers } from './hooks'
+
+type UserCreate = Parameters<Mutation['userRegister']>[0]
+
+const Page: React.FC = () => {
+  //const { isAuthenticated, user } = useAuthenticationContext()
+
+
+  const { users, isLoading } = useUsers()
+  return (
+    <>
+
+      <Stack spacing="4">
+        <Heading size="md">User ({users.length})</Heading>
+
+        <HStack spacing="4" justifyContent="end">
+          <AddUserControl />
+        </HStack>
+
+        <Table>
+          <Thead position="sticky" top={0} zIndex={1} borderColor="black">
+            <Tr my=".8rem">
+              <Th></Th>
+              <Th>E-Mail Address</Th>
+              <Th>Username</Th>
+              <Th>First Name</Th>
+              <Th>Last Name</Th>
+              <Th>Created at</Th>
+              <Th>Active</Th>
+              <Th>Admin</Th>
+              <Th></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {isLoading && (
+              // map with 5 rows to show loading
+              [...Array(3)].map((_, index) => (
+
+                <Tr>
+                  <Td>
+                    <Skeleton key={index} w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <Text fontSize="sm">{index + 1}</Text>
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <Text fontSize="sm">john.doe@snek.at</Text>
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <Text fontSize="sm">john.doe</Text>
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <Text fontSize="sm">John</Text>
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <Text fontSize="sm">Doe</Text>
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <Text fontSize="sm">Tue Jun 27 2023</Text>
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <CheckCircleIcon />
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <CheckCircleIcon />
+                    </Skeleton>
+                  </Td>
+                  <Td>
+                    <Skeleton w={'fit-content'} h={'fit-content'} isLoaded={!isLoading}>
+                      <IconButton
+                        as={Link}
+                        aria-label="Edit"
+                        icon={<EditIcon />}
+                        to={`/cms/user/`}
+                      />
+                    </Skeleton>
+                  </Td>
+                </Tr>
+              ))
+            )}
+
+            {users
+              .map((user, index) => (
+                <Tr key={user.id}>
+                  <Td>
+                    <Text fontSize="xs">{index + 1}</Text>
+                  </Td>
+                  <Td>
+                    <Text fontSize="sm">{user.primaryEmailAddress}</Text>
+                  </Td>
+                  <Td>
+                    <Text fontSize="sm">{user.username}</Text>
+                  </Td>
+                  <Td>
+                    <Text fontSize="sm">{user.details?.firstName}</Text>
+                  </Td>
+                  <Td>
+                    <Text fontSize="sm">{user.details?.lastName}</Text>
+                  </Td>
+                  <Td>
+                    <Text fontSize="sm">
+                      {new Date(user.createdAt).toDateString()}
+                    </Text>
+                  </Td>
+                  <Td>{user.isActive ? <CheckCircleIcon /> : null}</Td>
+                  <Td>{user.isAdmin ? <CheckCircleIcon /> : null}</Td>
+                  <Td textAlign={'right'}>
+                    <IconButton
+                      as={Link}
+                      aria-label="Edit"
+                      icon={<EditIcon />}
+                      to={`/cms/user/${user.id}`}
+                    />
+                  </Td>
+                </Tr>
+              ))
+              .reverse()}
+          </Tbody>
+        </Table>
+      </Stack >
+    </>
+  )
+}
+
+const PasswordInput = React.forwardRef<HTMLInputElement, any>(
+  ({ register, ...props }, ref) => {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+
+    return (
+      <InputGroup size="md">
+        <Input
+          autoComplete="new-password"
+          ref={ref}
+          pr="4.5rem"
+          type={show ? 'text' : 'password'}
+          {...props}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleClick}>
+            {show ? 'Hide' : 'Show'}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    )
+  }
+)
+
+
+
+const AddUserControl = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  //const navigate = useNavigate()
+
+  const { addUser } = useUsers()
+
+  const initialRef = React.useRef<HTMLInputElement | null>(null)
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting, isDirty, isValid }
+  } = useForm<UserCreate['values']>({})
+
+  const handleClose = () => {
+    reset()
+    onClose()
+  }
+
+  const onSubmit = async (values: UserCreate['values']) => {
+    const ok = await addUser(values)
+
+    if (ok) {
+      handleClose()
+      //navigate(0)
+    }
+  }
+
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={handleClose} initialFocusRef={initialRef}>
+        <ModalOverlay />
+        <ModalContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <ModalHeader>Add a user</ModalHeader>
+            <ModalCloseButton onClick={handleClose} />
+            <ModalBody pb={6}>
+              <FormControl isInvalid={!!errors.emailAddress}>
+                <FormLabel>E-Mail</FormLabel>
+                <Input
+                  placeholder="max.mustermann@snek.at"
+                  type="email"
+                  {...register('emailAddress', {
+                    required: 'This is required',
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: 'Invalid email address'
+                    }
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.emailAddress?.message}
+                </FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={!!errors.username}>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  placeholder="max.mustermann"
+                  {...register('username', {
+                    required: 'This is required'
+                  })}
+                />
+                <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
+              </FormControl>
+
+              <Stack direction={'row'}>
+                <Flex>
+                  <FormControl mt={4} isInvalid={!!errors.details?.firstName}>
+                    <FormLabel>Firstname</FormLabel>
+                    <Input
+                      placeholder="Max"
+                      {...register('details.firstName')}
+                    />
+                    <FormErrorMessage>
+                      {errors.details?.firstName?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </Flex>
+
+                <Flex>
+                  <FormControl mt={4} isInvalid={!!errors.details?.lastName}>
+                    <FormLabel>Lastname</FormLabel>
+                    <Input
+                      placeholder="Mustermann"
+                      {...register('details.lastName')}
+                    />
+                    <FormErrorMessage>
+                      {errors.details?.lastName?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </Flex>
+              </Stack>
+
+              <FormControl mt={4} isInvalid={!!errors.password}>
+                <FormLabel>Password</FormLabel>
+                <PasswordInput
+                  {...register('password', {
+                    required: 'This is required',
+                    pattern: {
+                      value:
+                        /^(?:(?=.*[a-z])(?:(?=.*[A-Z])(?=.*[\d\W])|(?=.*\W)(?=.*\d))|(?=.*\W)(?=.*[A-Z])(?=.*\d)).{8,}$/,
+                      message:
+                        'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character'
+                    }
+                  })}
+                />
+                <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+              </FormControl>
+            </ModalBody>
+
+            <ModalFooter>
+              <ButtonGroup isDisabled={!isDirty}>
+                <Button type="submit" isLoading={isSubmitting}>
+                  Create
+                </Button>
+                <Button variant="outline" onClick={handleClose}>
+                  Cancel
+                </Button>
+              </ButtonGroup>
+            </ModalFooter>
+          </form>
+        </ModalContent>
+      </Modal>
+
+      <Button leftIcon={<AddIcon />} onClick={onOpen}>
+        Add User
+      </Button>
+    </>
+  )
+}
+
+export default Page
+
+export const pageConfig: PageConfig = {
+  label: 'User',
+  icon: 'FaUser',
+  menu: {
+    type: 'app',
+    group: 'cms',
+    order: 500
+  },
+  layout: {
+    name: 'jaen'
+  },
+
+  breadcrumbs: [
+    {
+      label: 'CMS',
+      path: '/cms/'
+    },
+    {
+      label: 'User',
+      path: '/cms/users/'
+    }
+  ],
+  auth: {
+    isAdminRequired: true
+  }
+
+}
+
+export const query = graphql`
+      query ($jaenPageId: String!) {
+        ...JaenPageQuery
+    allJaenPage {
+        nodes {
+        ...JaenPageData
+        children {
+        ...JaenPageData
+      }
+      }
+    }
+  }
+      `
+
+export { Head } from '@atsnek/jaen'
+
