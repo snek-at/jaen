a1d090d72f2bd56cf158efacf340679d207af210
Nico Schett <nicoschett@icloud.com>
2023-09-23 15:11:49 +0200

    feat(gatsby-plugin-jaen): expose useJaenPagePaths

--- packages/gatsby-plugin-jaen/src/index.ts
+++ packages/gatsby-plugin-jaen/src/index.ts
@@ -1,3 +1,4 @@
 export {useJaenFrameMenuContext} from './contexts/jaen-frame-menu'
 export {CMSManagement} from './connectors/cms-management'
+export {useJaenPagePaths} from './gatsby/jaen-page-paths'
 export {jaenTheme as theme} from './theme/jaen-theme'
