2bf946f8201052975f2d7c57145084be82de7f95
Nico Schett <nicoschett@icloud.com>
2023-12-13 21:10:31 +0100

    chore: add linking example

--- my-gatsby-site/src/templates/BlogPage.tsx
+++ my-gatsby-site/src/templates/BlogPage.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react'
-import {Link, HeadFC, PageProps} from 'gatsby'
-import {Field, PageConfig, useJaenPageIndex} from '@atsnek/jaen'
+import {Link, HeadFC, PageProps, navigate} from 'gatsby'
+import {Field, PageConfig, useField, useJaenPageIndex} from '@atsnek/jaen'
+import {Button} from '@chakra-ui/react'
 
 const BlogPage: React.FC<PageProps> = props => {
   const index = useJaenPageIndex()
 
   console.log('index', index)
 
+  const field = useField('link', 'IMA:TextField')
+
   return (
     <main>
       {JSON.stringify(props.pageContext)}
       <Field.Text name="title" defaultValue="Test12345" />
+
+      <Field.Text name="link" defaultValue="/" />
+
+      <Button
+        onClick={() => {
+          navigate(field.value)
+        }}>
+        Navigate
+      </Button>
     </main>
   )
 }
